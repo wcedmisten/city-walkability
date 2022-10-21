1fbf9ea4494265849ec56e8761dcb22d6c75ccd9
William Edmisten <wcedmisten@gmail.com>
2022-10-20 20:04:21 -0400

    Format into markdown table

--- sort_results.py
+++ sort_results.py
@@ -9,7 +9,7 @@ my_rank = {}
 
 counter = 1
 for city, score in sorted(data.items(), key=lambda item: item[1], reverse=True):
-    print(city, ",", score, ",", score / max_score * 100)
+    print("|", counter, "|", city, "|", score, "|", score / max_score * 100, "|")
     my_rank[city] = counter
     counter += 1
 
